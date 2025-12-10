321ec09b2f45438ea652fd48e4a08e1278890d71
Dariusz Mikulski <dariusz.mikulski@gmail.com>
2025-12-10 13:58:59 +0100

    Wrapped output added

--- lib/custom_logger.dart
+++ lib/custom_logger.dart
@@ -26,32 +26,66 @@ class CustomLogger {
   log.Logger? internalLogger;
 
   void t(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger?.t('$loggerName: $message',
-        error: error, stackTrace: stackTrace);
+    internalLogger?.t(
+      '$loggerName: $message',
+      error: error,
+      stackTrace: stackTrace,
+    );
   }
 
   void d(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger?.d('$loggerName: $message',
-        error: error, stackTrace: stackTrace);
+    internalLogger?.d(
+      '$loggerName: $message',
+      error: error,
+      stackTrace: stackTrace,
+    );
   }
 
   void i(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger?.i('$loggerName: $message',
-        error: error, stackTrace: stackTrace);
+    internalLogger?.i(
+      '$loggerName: $message',
+      error: error,
+      stackTrace: stackTrace,
+    );
   }
 
   void w(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger?.w('$loggerName: $message',
-        error: error, stackTrace: stackTrace);
+    internalLogger?.w(
+      '$loggerName: $message',
+      error: error,
+      stackTrace: stackTrace,
+    );
   }
 
   void e(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger?.e('$loggerName: $message',
-        error: error, stackTrace: stackTrace);
+    internalLogger?.e(
+      '$loggerName: $message',
+      error: error,
+      stackTrace: stackTrace,
+    );
   }
 
   void f(dynamic message, [dynamic error, StackTrace? stackTrace]) {
-    internalLogger?.f('$loggerName: $message',
-        error: error, stackTrace: stackTrace);
+    internalLogger?.f(
+      '$loggerName: $message',
+      error: error,
+      stackTrace: stackTrace,
+    );
+  }
+}
+
+class WrapperConsoleOutput extends log.LogOutput {
+  void printWrapped(Object object) {
+    final pattern = RegExp('.{1,800}'); // 800 is the size of each chunk
+    pattern.allMatches('$object').forEach((match) async {
+      final line = match.group(0);
+      // ignore: avoid_print
+      print(line);
+    });
+  }
+
+  @override
+  void output(log.OutputEvent event) {
+    event.lines.forEach(printWrapped);
   }
 }
